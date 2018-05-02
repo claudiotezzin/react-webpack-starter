661e2f842637c6dfaadfd7912b7c286c3e8c01e5
Claudio Tezzin Jeremias <claudio.jeremias@clansoft.com.br>
2018-05-02 12:27:22 -0300

    materia-ui added

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,10 +1,38 @@
 import React from "react";
+import Button from "material-ui/Button";
+import { MuiThemeProvider, createMuiTheme } from "material-ui/styles";
+import AppBar from "material-ui/AppBar";
+import Toolbar from "material-ui/Toolbar";
+import Typography from "material-ui/Typography";
+import purple from "material-ui/colors/purple";
+import green from "material-ui/colors/green";
+import red from "material-ui/colors/red";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: purple,
+    secondary: green,
+    error: red
+  }
+});
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <h1>My React App</h1>
+        <MuiThemeProvider theme={theme}>
+          <AppBar position="fixed" color="primary">
+            <Toolbar>
+              <Typography variant="title" color="inherit">
+                Title
+              </Typography>
+            </Toolbar>
+          </AppBar>
+          <h1>My React App</h1>
+          <Button variant="raised" color="secondary">
+            Hello World
+          </Button>
+        </MuiThemeProvider>
       </div>
     );
   }
